d74a3de2d958e496511eccce2fe561100f8b585b
Burris19 <julianhernandez19@gmail.com>
2016-01-11 08:14:59 +0000

    [update] Seed with foreign key

--- database/seeds/BaseSeeder.php
+++ database/seeds/BaseSeeder.php
@@ -9,8 +9,11 @@
 use Faker\Factory as Faker;
 use Faker\Generator;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Database\Eloquent\Collection;
+use PhpSpec\Exception\Exception;
 abstract class BaseSeeder extends Seeder{
+
+    protected static $pool = array();
 
     protected function createMultiple($total, array $customValues = array())
     {
@@ -29,7 +32,50 @@ abstract class BaseSeeder extends Seeder{
 
         $values = array_merge($values, $customValues);
 
-        $this->getModel()->create($values);
+        return $this->addToPool($this->getModel()->create($values));
     }
 
+    protected function createFrom($seeder, array $customValues = array())
+    {
+        $seeder = new $seeder;
+        return $seeder->create($customValues);
+    }
+
+    protected function getRandom($model)
+    {
+        if ( ! $this->collectionExist($model))
+        {
+
+            throw new Exception("The $model collection does not exist");
+
+        }
+
+        return static::$pool[$model]->random();
+
+
+    }
+
+    protected function addToPool($entity)
+    {
+        $reflection = new ReflectionClass($entity);
+        $class = $reflection->getShortName();
+
+        if ( ! $this->collectionExist($class))
+        {
+            static::$pool[$class] = new Collection();
+        }
+
+        static::$pool[$class]->add($entity);
+
+        return $entity;
+    }
+
+    /**
+     * @param $class
+     * @return bool
+     */
+    protected function collectionExist($class)
+    {
+        return isset(static::$pool[$class]);
+    }
 }
