053d3e36f628c17f3534e06ac90c333210c559a4
Burris19 <julianhernandez19@gmail.com>
2016-01-12 08:08:47 +0000

    [update] template basic of the tickets and files the CSS

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -10,35 +10,35 @@ class TicketsController extends Controller {
 	public function latest()
     {
 
-        dd('latest');
+        return view('tickets/list');
 
     }
 
     public function popular()
     {
 
-        dd('popular');
+        return view('tickets/list');
 
     }
 
     public function open()
     {
 
-        dd('open');
+        return view('tickets/list');
 
     }
 
     public function closed()
     {
 
-        dd('closed');
+        return view('tickets/list');
 
     }
 
     public function details($id)
     {
 
-        dd('detail: ' . $id);
+        return view('tickets/details');
 
     }
 }
