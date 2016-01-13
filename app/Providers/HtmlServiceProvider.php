ed2a0cad5b6337a28ffdaff3da8b8ba166f2e4dd
Burris19 <julianhernandez19@gmail.com>
2016-01-13 07:04:25 +0000

    [create] Service Provider of the LaravelColective

--- app/Providers/HtmlServiceProvider.php
+++ app/Providers/HtmlServiceProvider.php
@@ -8,8 +8,20 @@
 
 namespace TeachMe\Providers;
 
-
-class HtmlServiceProvider
+use Collective\Html\HtmlServiceProvider as CollectiveHtmlServiceProvider;
+use TeachMe\Components\HtmlBuilder;
+class HtmlServiceProvider extends CollectiveHtmlServiceProvider
 {
-
+    /**
+     * Register the HTML builder instance.
+     *
+     * @return void
+     */
+    protected function registerHtmlBuilder()
+    {
+        $this->app->bindShared('html', function($app)
+        {
+            return new HtmlBuilder($app['url']);
+        });
+    }
 }
