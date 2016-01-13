e279c7df213bddd73af3b1f356ca0bf6c32777d7
Burris19 <julianhernandez19@gmail.com>
2016-01-13 07:05:05 +0000

    [create] extends HtmlBuider and implement class menu

--- app/Components/HtmlBuilder.php
+++ app/Components/HtmlBuilder.php
@@ -1,12 +1,19 @@
 <?php
 
+namespace TeachMe\Components;
 /**
  * Created by PhpStorm.
  * User: julian
  * Date: 13/01/16
  * Time: 12:33 AM
  */
-class HtmlBuilder
+
+use Collective\Html\HtmlBuilder as CollectiveHtmlBuilder;
+class HtmlBuilder extends CollectiveHtmlBuilder
 {
+    public function menu()
+    {
+        return view('partials/menu');
+    }
 
 }
