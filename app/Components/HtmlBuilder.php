d4feaa49dc4ceea38b41a7e91e82a6e6dfaca7c2
Burris19 <julianhernandez19@gmail.com>
2016-01-13 08:36:36 +0000

    [update] implement Dependency Injection

--- app/Components/HtmlBuilder.php
+++ app/Components/HtmlBuilder.php
@@ -7,13 +7,54 @@ namespace TeachMe\Components;
  * Date: 13/01/16
  * Time: 12:33 AM
  */
-
+use Illuminate\Contracts\Config\Repository as Config;
+use Illuminate\Contracts\View\Factory as View;
 use Collective\Html\HtmlBuilder as CollectiveHtmlBuilder;
+use Illuminate\Routing\UrlGenerator;
+
 class HtmlBuilder extends CollectiveHtmlBuilder
 {
-    public function menu()
+    private $config;
+    private $view;
+
+
+    public function __construct(Config $config, View $view, UrlGenerator $url)
     {
-        return view('partials/menu');
+        $this->config = $config;
+        $this->view = $view;
+        $this->url = $url;
+    }
+
+    public function menu($items)
+    {
+        if ( ! is_array($items))
+        {
+            $items = $this->config->get($items);
+        }
+        return $this->view->make('partials/menu', compact('items'));
+    }
+
+
+    public function classes(array $classes)
+    {
+        $html = '';
+
+        foreach ($classes as $name => $bool) {
+            if( is_int($name)) {
+                $name = $bool;
+                $bool = true;
+            }
+
+            if ($bool) {
+                $html .=$name.' ';
+            }
+        }
+
+        if ( ! empty($html)) {
+            return ' class="'.trim($html).'"';
+        }
+
+        return '';
     }
 
 }
