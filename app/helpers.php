71861210cc8ea2c77667c906ef671837db766d2e
Burris19 <julianhernandez19@gmail.com>
2016-01-18 06:37:24 +0000

    [add] get user auht

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,11 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: julian
- * Date: 17/01/16
- * Time: 10:44 PM
+ * @return \Illuminate\Contracts\Auth\Authenticatable|null
  */
+function currentUser()
+{
+
+    return auth()->user();
+
+}
